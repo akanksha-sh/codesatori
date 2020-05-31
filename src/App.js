a0cf3483dff2a1715206207156d5737987775f6b
Luke  Thorpe <luke.thorpe18@imperial.ac.uk>
2020-05-31 18:42:47 +0100

    Implemented basic skeleton for home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import LogIn from "./components/pages/LogIn";
+import SignUp from "./components/pages/SignUp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          CodeSatori is coming soon.
-        </p>
-        <div className="App-subtitle">
-          <p>
-            Enlightenment is just around the corner...
-          </p>
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App" style={appStyle}>
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <h1>Home</h1>
+                  <p>Welcome to CodeSatori!</p>
+                  <p>Sorry, but we're currently working on the homepage...</p>
+                  <p>
+                    Feel free to email us at contact.codesatori@gmail.com with
+                    any suggestions.
+                  </p>
+                </React.Fragment>
+              )}
+            />
+            <Route path="/login" component={LogIn} />
+            <Route path="/signup" component={SignUp} />
+          </div>
         </div>
-        <audio autoPlay loop>
-              <source src="/waves2.mp3" />
-        </audio>
-      </header>
-    </div>
-  );
+      </Router>
+    );
+  }
 }
 
-export default App;
+const appStyle = {
+  fontFamily: "Roboto",
+  padding: "10px",
+  textAlign: "left",
+};
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SignUp() {
+  return (
+    <React.Fragment>
+      <h1>Sign Up</h1>
+      <p>This is the page where you sign-up...</p>
+    </React.Fragment>
+  );
+}
