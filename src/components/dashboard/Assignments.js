580ccf5faa0490b70359af9e0920e7e175db3240
Akanksha <as16418@ic.ac.uk>
2020-06-05 00:02:25 +0100

    [as16418] Add functionality to add assignments, set deadline for assignments in assignments tab

--- src/components/dashboard/Assignments.js
+++ src/components/dashboard/Assignments.js
@@ -9,12 +9,32 @@ export default class Assignments extends Component {
     super(props);
   }
 
+  addAssignment = (title) => {
+    const newAssignment = {
+      id: 7,
+      title,
+      ongoing: true,
+    }
+    this.setState({assignments: [...this.state.assignments, newAssignment]})
+  }
+
   render() {
     const user = this.context;
 
     if (user.isTeacher) {
       return (
+<<<<<<< HEAD
         <TeacherAssignments />
+=======
+        <div style={contentDiv}>
+          <h2 style={pageTitle}> Assignments </h2>
+          <br/>
+          <AddAssignment addAssignment={this.addAssignment}/> 
+          <br/>
+          <br/>
+          <TeacherAssignments classes={this.state.assignments} />
+        </div>
+>>>>>>> a82a0cb... [as16418] Add functionality to add assignments, set deadline for assignments in assignments tab
       );
     }
     return <div></div>;
